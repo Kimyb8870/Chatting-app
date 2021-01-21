51c1c5409841b8dca45dfff5f8cceaa386d0480f
kimyb8870 <qhd0084@gmail.com>
2021-01-22 06:48:14 +0900

    Add: firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDk9fUUxDGGAO6BQ1URo3mI8psnQkddn1M",
+  authDomain: "chatting-app-1ab58.firebaseapp.com",
+  projectId: "chatting-app-1ab58",
+  storageBucket: "chatting-app-1ab58.appspot.com",
+  messagingSenderId: "184400940803",
+  appId: "1:184400940803:web:fccf3597a2b1a1b7a0e336",
+  measurementId: "G-QF0Q1FF342",
+};
+
+firebase.initializeApp(firebaseConfig);
